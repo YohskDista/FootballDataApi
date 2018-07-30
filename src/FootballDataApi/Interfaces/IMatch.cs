c33e716a49a47ef530fc46dadcecd113796de6d6
YohskDista <yohsk10@gmail.com>
2018-07-30 19:27:48 +0200

    Create the interface for the matches, to complete

---
+++ src/FootballDataApi/Interfaces/IMatch.cs
@@ -0,0 +1,17 @@
+﻿using FootballDataApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballDataApi.Interfaces
+{
+    public interface IMatch
+    {
+        Task<IEnumerable<Match>> GetAllMatchFrom(int idCompetition, params string[] filters);
+
+        Task<IEnumerable<Match>> GetAllMatchOfTeam(int idTeam, params string[] filters);
+
+        Task<Match> GetMatchById(int idMatch);
+    }
+}
