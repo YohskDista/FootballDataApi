df7af65f8dc365950911f12dc648fceb06b6c68a
YohskDista <yohsk10@gmail.com>
2018-07-30 09:19:51 +0200

    Add project for request to API and Competition controller

---
+++ FootballDataApi.Request/CompetitionController.cs
@@ -0,0 +1,25 @@
+﻿using FootballDataApi.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballDataApi.Request
+{
+    public class CompetitionController
+    {
+        public CompetitionController()
+        {
+
+        }
+
+        public IEnumerable<Competition> GetAvailableCompetition()
+        {
+            return null;
+        }
+
+        public Competition GetCompetition(int id)
+        {
+            return null;
+        }
+    }
+}
