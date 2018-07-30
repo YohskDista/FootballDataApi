d0ae9d16f8ef6b0cad6aeeb0ce6925b831879ac8
YohskDista <yohsk10@gmail.com>
2018-07-30 09:32:50 +0200

    Add interface to abstract the data source of the competition

---
+++ FootballDataApi.Request/Interfaces/ICompetition.cs
@@ -0,0 +1,13 @@
+﻿using FootballDataApi.Domain;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FootballDataApi.Request.Interfaces
+{
+    public interface ICompetition
+    {
+        Task<IEnumerable<Competition>> GetAvailableCompetition();
+
+        Task<Competition> GetCompetition(int id);
+    }
+}
