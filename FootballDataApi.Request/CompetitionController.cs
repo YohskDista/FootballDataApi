effd99273f7a1a699454518f1439b9d0d073e7d5
YohskDista <yohsk10@gmail.com>
2018-07-30 09:32:13 +0200

    Implement the two methods get all and get competition

--- FootballDataApi.Request/CompetitionController.cs
+++ FootballDataApi.Request/CompetitionController.cs
@@ -1,25 +1,44 @@
 ﻿using FootballDataApi.Domain;
+using FootballDataApi.Request.Interfaces;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace FootballDataApi.Request
 {
-    public class CompetitionController
+    public class CompetitionController : ICompetition
     {
-        public CompetitionController()
-        {
+        private readonly HttpClient _httpClient;
 
+        public CompetitionController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
         }
 
-        public IEnumerable<Competition> GetAvailableCompetition()
+        public async Task<IEnumerable<Competition>> GetAvailableCompetition()
         {
-            return null;
+            var request = new HttpRequestMessage(HttpMethod.Get, "http://api.football-data.org/v2/competitions");
+            return await Get<IEnumerable<Competition>>(_httpClient, request);
         }
 
-        public Competition GetCompetition(int id)
+        public async Task<Competition> GetCompetition(int id)
         {
-            return null;
+            var request = new HttpRequestMessage(HttpMethod.Get, $"http://api.football-data.org/v2/competitions/{id}");
+            return await Get<Competition>(_httpClient, request);
+        }
+
+        private async Task<T> Get<T>(HttpClient httpClient, HttpRequestMessage request)
+        {
+            using (var response = await httpClient.SendAsync(request, new CancellationToken()))
+            {
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<T>(content);
+            }
         }
     }
 }
