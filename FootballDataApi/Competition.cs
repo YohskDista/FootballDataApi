7e50355a14d3cf458d3b804b64497ab01e88db5a
YohskDista <yohsk10@gmail.com>
2018-07-30 08:00:16 +0200

    Add DTO objects to represent result of the API
    Create the basic classes that we need for the competition, the club
    and the matchs request. Make some adjustement and aggregate the
    classes that can be

---
+++ FootballDataApi/Competition.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballDataApi
+{
+    public class Competition
+    {
+        public int Id { get; set; }
+        public Area Area { get; set; }
+        public string Name { get; set; }
+        public object Code { get; set; }
+        public string Plan { get; set; }
+        public Season CurrentSeason { get; set; }
+        public IEnumerable<Season> Seasons { get; set; }
+        public int NumberOfAvailableSeasons { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+
+    public class Area
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Season
+    {
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int CurrentMatchday { get; set; }
+        public string[] AvailableStages { get; set; }
+    }
+}
---
+++ FootballDataApi/Match.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballDataApi
+{
+    public class Match
+    {
+        public int Id { get; set; }
+        public Competition Competition { get; set; }
+        public Season Season { get; set; }
+        public DateTime UtcDate { get; set; }
+        public string Status { get; set; }
+        public object Minute { get; set; }
+        public int Attendance { get; set; }
+        public int Matchday { get; set; }
+        public string Stage { get; set; }
+        public string Group { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public MatchTeam HomeTeam { get; set; }
+        public MatchTeam AwayTeam { get; set; }
+        public Score Score { get; set; }
+        public Goal[] Goals { get; set; }
+        public Booking[] Bookings { get; set; }
+        public Substitution[] Substitutions { get; set; }
+        public Referee[] Referees { get; set; }
+    }
+
+    public class Coach
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CountryOfBirth { get; set; }
+        public string Nationality { get; set; }
+    }
+
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public int ShirtNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string CountryOfBirth { get; set; }
+        public string Nationality { get; set; }
+        public string Role { get; set; }
+    }
+
+    public class MatchTeam
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Coach Coach { get; set; }
+        public Player Captain { get; set; }
+        public Player[] Lineup { get; set; }
+        public Player[] Bench { get; set; }
+    }
+
+    public class Score
+    {
+        public string Winner { get; set; }
+        public string Duration { get; set; }
+        public Halftime HalfTime { get; set; }
+        public Fulltime FullTime { get; set; }
+        public Extratime ExtraTime { get; set; }
+        public Penalties Penalties { get; set; }
+    }
+
+    public class Halftime
+    {
+        public int HomeTeam { get; set; }
+        public int AwayTeam { get; set; }
+    }
+
+    public class Fulltime
+    {
+        public int HomeTeam { get; set; }
+        public int AwayTeam { get; set; }
+    }
+
+    public class Extratime
+    {
+        public object HomeTeam { get; set; }
+        public object AwayTeam { get; set; }
+    }
+
+    public class Penalties
+    {
+        public object HomeTeam { get; set; }
+        public object AwayTeam { get; set; }
+    }
+
+    public class Goal
+    {
+        public int minute { get; set; }
+        public Player scorer { get; set; }
+        public Player[] assist { get; set; }
+    }
+
+    public class Booking
+    {
+        public int Minute { get; set; }
+        public MatchTeam Team { get; set; }
+        public Player Player { get; set; }
+        public string Card { get; set; }
+    }
+
+    public class Substitution
+    {
+        public int Minute { get; set; }
+        public MatchTeam Team { get; set; }
+        public Player PlayerOut { get; set; }
+        public Player PlayerIn { get; set; }
+    }
+
+    public class Referee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public object Nationality { get; set; }
+    }
+
+}
