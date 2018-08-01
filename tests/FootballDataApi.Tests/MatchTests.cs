ed2efeb876db30c7a8bcb6e52889fe6c880c49b0
YohskDista <yohsk10@gmail.com>
2018-08-01 16:21:11 +0200

    Add a test project to the solution

---
+++ tests/FootballDataApi.Tests/MatchTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballDataApi.Tests
+{
+    [TestFixture]
+    public class MatchTests
+    {
+        [Test]
+        public void AddFiltersToUrl_With_GivenFilters_MustReturn_CorrectUrl()
+        {
+            
+        }
+    }
+}
