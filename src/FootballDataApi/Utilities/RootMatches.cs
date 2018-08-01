e0c8bd247e2af1110d1820a4b61e730943aa2a6d
YohskDista <yohsk10@gmail.com>
2018-08-01 19:48:20 +0200

    Add json for the match data to test

---
+++ src/FootballDataApi/Utilities/RootMatches.cs
@@ -0,0 +1,14 @@
+﻿using FootballDataApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballDataApi.Utilities
+{
+    public sealed class RootMatches
+    {
+        public int Count { get; set; }
+        public object Filters { get; set; }
+        public IEnumerable<Match> Matches { get; set; }
+    }
+}
