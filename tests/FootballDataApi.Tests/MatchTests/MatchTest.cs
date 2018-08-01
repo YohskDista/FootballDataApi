79620304699563b77ffae78224270d9488047dbf
YohskDista <yohsk10@gmail.com>
2018-08-01 16:58:43 +0200

    Rename MatchTests to MatchTest

--- tests/FootballDataApi.Tests/MatchTests/MatchTest.cs
+++ tests/FootballDataApi.Tests/MatchTests/MatchTest.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FootballDataApi.Tests
+namespace FootballDataApi.Tests.MatchTests
 {
     [TestFixture]
     public class MatchTests
