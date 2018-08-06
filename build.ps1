dc03f71bd5bb635d8a8f0994928e6ab0782d09ed
YohskDista <yohsk10@gmail.com>
2018-08-06 19:51:07 +0200

    Change version suffix in build, not support -[numeric]

--- build.ps1
+++ build.ps1
@@ -64,7 +64,7 @@ exec {& dotnet restore .\tests\FootballDataApi.Tests}
 exec {& dotnet build .\src\FootballDataApi\FootballDataApi.csproj }
 
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
+$revision = "build{0:D4}" -f [convert]::ToInt32($revision, 10)
 
 'Running tests' | Write-Verbose
 exec { & dotnet test .\tests\FootballDataApi.Tests\FootballDataApi.Tests.csproj -c Release }
